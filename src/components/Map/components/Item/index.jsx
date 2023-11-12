de9cd1f2d052647eba37351edd97527ffb18b29f
Alex Chirkin <hello@alexchirkin.me>
2023-11-13 00:59:03 +0200

    default icon and title added for map item

--- src/components/Map/components/Item/index.jsx
+++ src/components/Map/components/Item/index.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import { useGrid } from '../../../../hooks/useGrid'
 import { useItem } from '../../../../hooks/useItem'
 import './style.css'
 
 export function Item({ item, x, y, className }) {
   const { width, height, id } = useItem(item)
+  const [isError, setIsError] = useState(false)
+
   const { getAnchor } = useGrid()
   const coords = getAnchor(x, y)
+
   return (
     <img
-      src={`/images/${id}.png`}
+      title={id}
+      src={isError ? '/images/default.png' : `/images/${id}.png`}
+      onError={() => setIsError(true)}
       style={{
         width: width + 'px',
         height: height + 'px', 
